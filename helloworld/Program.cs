ebcb7432be20a7edb60e7b1bd2caf81ea790e17e
k33d4 <ashishdbatho@gmail.com>
2024-02-24 23:27:15 +0530

    Initial Commit
    Initial Commit

---
+++ helloworld/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace helloworld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int hoursToAdd, minutesToAdd, secondsToAdd;
+        hoursToAdd = minutesToAdd = secondsToAdd = 0;
+        string isChangeReuired = "N";
+        Console.WriteLine("Current Date and Time is: " + DateTime.Now.ToString("MM/dd/yy hh:mm:ss tt"));
+        Console.Write("Enter Hours to Add: ");
+        hoursToAdd = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Minutes to Add: ");
+        minutesToAdd = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Seconds to Add: ");
+        secondsToAdd = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Updated Date and Time is: " 
+                + DateTime.Now
+                .AddHours(hoursToAdd)
+                .AddMinutes(minutesToAdd)
+                .AddSeconds(secondsToAdd)
+                .ToString("dd-MMM-yyyy HH:mm:ss")
+            );
+
+        Console.Write("Do you want to change the date again ?");
+        isChangeReuired = (Console.ReadLine());
+        if(isChangeReuired.ToLower() == "y" ){
+            Console.Write("Enter Hours to Add: ");
+            hoursToAdd = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Minutes to Add: ");
+            minutesToAdd = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Seconds to Add: ");
+            secondsToAdd = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Updated Date and Time is: " 
+                    + DateTime.Now
+                    .AddHours(hoursToAdd)
+                    .AddMinutes(minutesToAdd)
+                    .AddSeconds(secondsToAdd)
+                    .ToString("dd-MMM-yyyy HH:mm:ss")
+                );
+        }
+
+        //For
+        //While
+        //Do-While
+        for(int i = 0; i<5;i++){
+            Console.Write("Enter Hours to Add: ");
+            hoursToAdd = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Minutes to Add: ");
+            minutesToAdd = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Seconds to Add: ");
+            secondsToAdd = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Updated Date and Time is: " 
+                    + DateTime.Now
+                    .AddHours(hoursToAdd)
+                    .AddMinutes(minutesToAdd)
+                    .AddSeconds(secondsToAdd)
+                    .ToString("dd-MMM-yyyy HH:mm:ss")
+                );
+        }
+    }
+}
